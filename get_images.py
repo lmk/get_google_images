7e40efc923b5334ad8fd61c4189ec7b2e2a85ea9
lmk <newtypez@gmail.com>
2022-09-28 16:21:35 +0900

    for python3

--- get_images.py
+++ get_images.py
@@ -1,44 +1,69 @@
 #-*- coding: utf-8 -*-
-import urllib2
 import os
 from bs4 import BeautifulSoup
-import json
-import sys
+import requests
+import validators
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
+DIR = "./Pictures/"
 
-DIR= "./Pictures/"
+QUERY = "안구정화"
 
-query = "안구정화"
-query= str(unicode(query))
-query=  '+'.join(query.split())
-url="https://www.google.co.in/search?q="+query+"&source=lnms&tbm=isch"
-header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36',
 }
-req= urllib2.Request(url,headers=header)
-soup= urllib2.urlopen(req)
-soup= BeautifulSoup(soup, "lxml")
 
-ActualImages=[]# contains the link for Large original images, type of  image
-for a in soup.find_all("div",{"class":"rg_meta"}):
-    link , Type =json.loads(a.text)["ou"]  ,json.loads(a.text)["ity"]
-    ActualImages.append((link,Type))
- 
-for i , (img , Type) in enumerate( ActualImages):
+if not os.path.exists(DIR):
+    os.mkdir(DIR)
+
+QUERY =  '+'.join(QUERY.split())
+url ="https://www.google.co.in/search?q="+QUERY+"&source=lnms&tbm=isch"
+
+res = requests.get(url, timeout=3, headers=HEADERS)
+
+if res.status_code != 200:
+    print("Error: ", res.status_code)
+    quit()
+
+soup = BeautifulSoup(res.text, "html.parser")
+
+for h1 in soup.find_all("h1"):
+    if h1.text == "Search results":
+        soup = h1.findNext("div")
+        break
+
+i = 1
+for img in soup.find_all("img"):
+    if "src" in img.attrs:
+        url = img.attrs["src"]
+    elif "data-src" in img.attrs:
+        url = img.attrs["data-src"]
+    
+    if not validators.url(url):
+        continue
+
     try:
-        req = urllib2.Request(img, headers={'User-Agent' : header})
-        raw_img = urllib2.urlopen(req).read()
-        if not os.path.exists(DIR):
-            os.mkdir(DIR)
-        cntr = i + 1
-        print str(cntr)
-        if len(Type)==0:
-            f = open(DIR + query + "_"+ str(cntr)+".jpg", 'wb')
-        else:
-            f = open(DIR + query + "_"+ str(cntr)+"."+Type, 'wb')
-        f.write(raw_img)
-        f.close()
+        res = requests.get(url, timeout=3, headers=HEADERS)
+        if res.status_code != 200:
+            continue
+
+        resType = ""
+        if "Content-Type" in res.headers:
+            resType = res.headers["Content-Type"]
+
+        imgType = ""
+        for t in ["jpeg", "svg", "png", "gif", "bmp"]:
+            if t in resType:
+                imgType = t
+                break
+
+        if imgType == "":
+            continue
+
+        fn = QUERY + "_{0:03d}.".format(i) + imgType
+        print("download. ", fn)
+        open(DIR + "/" + fn, "wb").write(res.content)
+        i = i + 1
+
     except Exception as e:
-        print "could not load : "+img
-        #print e
+        print("could not load : " + img)
+ 
